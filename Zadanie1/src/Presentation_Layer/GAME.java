2a27fcb79fb73deb702343103d5406ceba944cb7
drivaldo333 <drivaldo333@gmail.com>
2017-06-01 18:59:45 +0200

    Dodano klase Background, która ładuje zdjęcie do tła okna.

--- Zadanie1/src/Presentation_Layer/GAME.java
+++ Zadanie1/src/Presentation_Layer/GAME.java
@@ -1,21 +1,31 @@
 package Presentation_Layer;
 
-import java.awt.FlowLayout;
-
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class GAME {
 	
-	public GAME() {
-
-		JFrame jframe = new JFrame("GRA");
-		jframe.setLayout(new FlowLayout());
-		jframe.setSize(800, 600);
-		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JLabel jlabel = new JLabel("Swing w Javie.");
-		jframe.add(jlabel);
-		jframe.setVisible(true);
-
+	private JPanel panel1;
+	private JFrame frame1;
+	
+	public GAME(){
+		
+		frame1 = new JFrame();
+		frame1.setVisible(true);
+		frame1.setResizable(false);
+		frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame1.setSize(800,400);
+		frame1.setName("TAMAGOTCHI");
+		
+		panel1 = new JPanel(new GridBagLayout());
+		
+		new GridBagConstraints();
+		
+		frame1.setContentPane(new Background("GAMEbackground.jpg"));
+		
+		frame1.add(panel1);	
 	}
+
 }
--- Zadanie1/src/Presentation_Layer/LOG.java
+++ Zadanie1/src/Presentation_Layer/LOG.java
@@ -1,36 +1,69 @@
 package Presentation_Layer;
 
-import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class LOG extends JFrame {
-
-	public LOG() {
-
-		setResizable(false);
-		JFrame jframe = new JFrame("LOGOWANIE");
-		jframe.setLayout(new FlowLayout());
-		jframe.setSize(250, 350);
-		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jframe.setVisible(true);
-		setLocationRelativeTo(null);
+public class LOG {
+	
+	private JFrame frame1;
+	private JPanel panel1;
+	private JButton button1;
+	private JLabel label1;
+	private JTextField text1;
+	private JLabel label2;
+	private JTextField text2;
+	
+	public LOG(){
 		
-		JLabel jlabelname = new JLabel("User name: ");
-		jframe.add(jlabelname);	
-		JTextField jtextfieldname = new JTextField(10); 
-		jframe.add(jtextfieldname);
+		frame1 = new JFrame();
+		frame1.setVisible(true);
+		frame1.setResizable(false);
+		frame1.setSize(300,400);
+		frame1.setName("LOGOWANIE");
 		
-		JLabel jlabelpassword = new JLabel("Password: ");
-		jframe.add(jlabelpassword);
-		JTextField jtextfieldpassword = new JTextField(10); 
-		jframe.add(jtextfieldpassword);
+		panel1 = new JPanel(new GridBagLayout());
+		panel1.setOpaque(false);
 		
-		JButton jbutton = new JButton("Logowanie");  
-		jframe.add(jbutton); 
-
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		
+		frame1.setContentPane(new Background("LOGbackground.jpg"));
+		
+		c.gridx = 0;
+		c.gridy = 0;
+		label1 = new JLabel("User name: ");
+		panel1.add(label1, c);
+		
+		c.gridx = 1;
+		c.gridy = 0;
+		text1 = new JTextField(15);
+		panel1.add(text1, c);
+		
+		c.gridx = 0;
+		c.gridy = 1;
+		label2 = new JLabel("Password: ");
+		panel1.add(label2, c);
+		
+		c.gridx = 1;
+		c.gridy = 1;
+		text2 = new JTextField(15);
+		panel1.add(text2, c);
+		
+		c.gridx = 0;
+		c.gridy = 2;
+		c.gridwidth = 2;
+		c.insets = new Insets(20,0,0,0);
+		button1 = new JButton("Zaloguj");
+		panel1.add(button1,c);	
+		
+		frame1.add(panel1);		
 	}
 
 }
